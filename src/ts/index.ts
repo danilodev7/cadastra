80163be49eb2260eb244b3d5d431fd64c4e6a4e9
danilodev7 <danilodev7@gmail.com>
2025-01-10 15:33:33 -0300

    :sparkles: feat: Arquivo principal que inicializa a aplicação.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,32 @@
-import { Product } from "./Product";
+import { setupCart } from "../components/setupCart";
+import { renderProducts } from '../components/renderProducts';
+import { setupFilters } from '../components/setupFilters';
+import { setupCartModal } from "../components/setupCartModal";
+import { setupFilterModal } from "../components/setupFilterModal";
+import { setupSortModal } from "../components/setupSortModal";
+import { fetchProducts } from './api';
 
-const serverUrl = "http://localhost:5000";
+let currentPage = 1;
 
 function main() {
-  console.log(serverUrl);
+  fetchProducts().then(products => {
+    console.log('Carregando Produtos...'); 
+    renderProducts(products);
+    setupFilters(products);
+    setupFilterModal(products);
+    setupCartModal();
+    setupSortModal(products);
+    setupCart();
+  });
+  
+
+  document.getElementById('load-more')?.addEventListener('click', () => {
+    currentPage++;
+    fetchProducts(currentPage).then(products => {
+      console.log('Carregando mais produtos:', products);
+      renderProducts(products, true);
+    });
+  });
 }
 
 document.addEventListener("DOMContentLoaded", main);
